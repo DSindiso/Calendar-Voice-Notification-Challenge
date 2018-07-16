6adc7e5f8aba28fe847950f1f7ee56da983b5bcd
Sindiso <sindiso@finchatbot.com>
2018-07-16 09:55:18 +0200

    group all events by dates

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
+const moment = require('moment');
+const underscore = require('underscore');
 
-// If modifying these scopes, delete credentials.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 const TOKEN_PATH = 'credentials.json';
 
-// Load client secrets from a local file.
 try {
-  const content = fs.readFileSync('client_secret.json');
-  authorize(JSON.parse(content), listEvents);
-} catch (err) {
-  return console.log('Error loading client secret file:', err);
+    const content = fs.readFileSync('user_auth.json');
+    authorize(JSON.parse(content), listEvents);
+} catch (error) {
+    console.log('Error loading user details: ', error)
 }
 
 /**
@@ -21,20 +21,21 @@ try {
  * @param {function} callback The callback to call with the authorized client.
  * @return {function} if error in reading credentials.json asks for a new one.
  */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  let token = {};
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
-  try {
-    token = fs.readFileSync(TOKEN_PATH);
-  } catch (err) {
-    return getAccessToken(oAuth2Client, callback);
-  }
-  oAuth2Client.setCredentials(JSON.parse(token));
-  callback(oAuth2Client);
+function authorize(credintials, callback) {
+    const {client_secret, client_id, redirect_uris} = credintials.installed;
+    let token = {};
+    const oAuth2Client = new google.auth.OAuth2 (
+        client_id, client_secret, redirect_uris[0]);
+
+    //Check if we have previously stored a token.
+    try {
+        token = fs.readFileSync(TOKEN_PATH);
+    } catch (error) {
+        return getAccessToken(oAuth2Client, callback)
+    }
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
 }
 
 /**
@@ -43,31 +44,60 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return callback(err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      try {
-        fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
-        console.log('Token stored to', TOKEN_PATH);
-      } catch (err) {
-        console.error(err);
-      }
-      callback(oAuth2Client);
+let getAccessToken = (oAuth2Client, callback) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
     });
-  });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) =>{
+            if (err) return callback(err);
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            try {
+                fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
+                console.log('Token stored to', TOKEN_PATH);
+            } catch (error) {
+                console.log(error)
+            }
+            callback(oAuth2Client);
+        });
+    });
+}
+
+let getTime = (hours, min) => {
+    try {
+        let evaluateTime = hours < 12 ? `${hours}:${min}am` : `${hours}:${min}pm`;
+        return evaluateTime
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+let compareDate = (calendarEvents) => {
+    let firstDate = new Date(calendarEvents[0].start);
+    let groupCalenderEvents = underscore.groupBy(calendarEvents, 'start');
+    console.log(groupCalenderEvents);
+    // calendarEvents.map((event, i) => {
+    //     let eventDate = moment(event.start).format('YYYY/MM/DD');
+    //     let temp = moment(firstDate).format('YYYY/MM/DD');
+    //     if (firstDate < event.start) {
+    //         console.log(temp, '   ', eventDate)
+    //         console.log('date match range')
+    //         firstDate = event.start;
+    //     } else
+    //         console.log('does not match range')
+    //     // console.log(temp.valueOf()) 
+    //     // console.log(moment(temp).format('YYYY/MM/DD'));
+    // });
+    // console.log(calendarEvents)
+    // console.log(`${startTime}-${endTime}\t\t${summary}\t${location}`);
 }
 
 /**
@@ -75,24 +105,31 @@ function getAccessToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-  });
+    const calendar = google.calendar({version: 'v3', auth});
+    calendar.events.list({
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        singleEvents: true,
+        orderBy: 'startTime',
+    }, (error, res) => {
+        if (error) return console.log('The API returned an error: ' + err);
+        const events = res.data.items;
+        const calendarEvents = [];
+        if (events.length) {
+            console.log('Upcoming events:');
+            events.map((event, i) => {
+                let start = new Date(event.start.dateTime);
+                const end = new Date(event.end.dateTime);
+                const startTime = getTime(start.getHours(), start.getMinutes());
+                const endTime = getTime(end.getHours(), end.getMinutes());
+                const summary = event.summary ? event.summary : '';
+                const location = event.location ? event.location : '';
+                start = moment(start).format('ddd MMM DD, YYYY');
+                calendarEvents.push({start, end, startTime, endTime, summary, location});
+            });
+            compareDate(calendarEvents);
+        } else {
+            console.log('No upcoming events found.')
+        }
+    });
 }
